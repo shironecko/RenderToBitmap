a89f24af4442a290fd4026a39a8d8207f104ca1d
Shironecko <shironecko.mail@gmail.com>
2015-08-06 22:22:50 +0300

    Add a simple implementation of Bresenham's Line Algorithm

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,53 @@
 #include <iostream>
+#include <algorithm>
 #include "bitmap.h"
 
 using namespace std;
 
-void line(Bitmap& image, uint32 x0, uint32 y0, uint32 x1, uint32 y1, uint8 r, uint8 g, uint8 b);
+void line(Bitmap& image, int32 x0, int32 y0, int32 x1, int32 y1, uint8 r, uint8 g, uint8 b);
+
 int main()
 {
   Bitmap image(200, 100);
-  line(image, 10, 10, 190, 90, 0, 0, 255);
+  line(image, 10, 10, 40, 90, 0, 255, 0);
+  line(image, 50, 90, 20, 10, 255, 0, 0);
+  line(image, 100, 10, 190, 40, 0, 0, 255);
+  line(image, 180, 40, 90, 10, 0, 255, 255);
+  line(image, 50, 80, 80, 10, 255, 255, 0);
+  line(image, 90, 10, 60, 80, 255, 0, 255);
   image.Serialize(&cout);
 }
 
 void line(Bitmap& image,
-          uint32 x0, uint32 y0, uint32 x1, uint32 y1,
-          uint8 r, uint8 g, uint8 b)
+          int32 x0, int32 y0, int32 x1, int32 y1,
+          uint8 r, uint8 g, uint8 b) 
 {
-  const float step = 0.01f;
-  for (float t = 0; t < 1.0f; t += step)
+  bool steep = false;
+  if (abs(x0 - x1) < abs(y0 - y1)) 
   {
-    image.SetPixel(uint32(x0 * (1.0f - t) + x1 * t),
-                   uint32(y0 * (1.0f - t) + y1 * t),
-                   r, g, b);
+    std::swap(x0, y0);
+    std::swap(x1, y1);
+    steep = true;
+  }
+
+  if (x0>x1) 
+  {
+    std::swap(x0, x1);
+    std::swap(y0, y1);
+  }
+
+  for (int32 x=x0; x<=x1; x++) 
+  {
+    float t = (x - x0) / (float)(x1 - x0);
+    int32 y = y0 * (1.0f - t) + y1 * t;
+    if (steep) 
+    {
+      image.SetPixel(y, x, r, g, b);
+    } 
+    else 
+    {
+      image.SetPixel(x, y, r, g, b);
+    }
   }
 }
+
