6ed2eb53538d2ac2228d5a5aba067847ed464819
Shironecko <shironecko.mail@gmail.com>
2015-08-12 19:05:44 +0300

    Add a script that automates testing of the render

---
+++ source/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Compile RenderToBitmap project and launch it, piping
+# the output to the feh image viewer
+mkdir ../build
+pushd ../build
+cmake ../source && make
+./RenderToBitmap < ../data/head.obj | feh --force-aliasing -
+popd
