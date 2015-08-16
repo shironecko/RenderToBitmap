772b1f5009ddb6ca67947709cbfce75efd42d019
Shironecko <shironecko.mail@gmail.com>
2015-08-16 18:14:36 +0300

    Add a matrix class and update CMakeLists to support MSVC, GCC or Clang only

---
+++ source/matrix.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cmath>
+#include <cassert>
+#include "types.h"
+#include "vec.h"
+
+template<uint32 TWidth, uint32 THeight, typename T>
+class matrix
+{
+  vec<TWidth, T> m_rows[THeight];
+
+public:
+  matrix() { }
+
+  vec<TWidth, T>& operator[] (uint32 index)
+  {
+    assert(index < THeight);
+    return m_rows[index];
+  }
+
+  const vec<TWidth, T>& operator[] (uint32 index) const
+  {
+    assert(index < THeight);
+    return m_rows[index];
+  }
+
+  vec<THeight, T> column(uint32 index) const
+  {
+    assert(index < TWidth);
+    vec<THeight, T> result;
+    for (uint32 i = 0; i < THeight; ++i)
+    {
+      result[i] = m_rows[i][index];
+    }
+
+    return result;
+  }
+
+  void setColumn(uint32 index, vec<THeight, T> v)
+  {
+    assert(index < TWidth);
+    for (uint32 i = 0; i < THeight; ++i)
+    {
+      m_rows[i][index] = v[i];
+    }
+  }
+
+  static matrix<THeight, TWidth, T> identity()
+  {
+    matrix<THeight, TWidth, T> result;
+    for (uint32 y = 0; y < THeight; ++y)
+    {
+      for (uint32 x = 0; x < TWidth; ++x)
+      {
+        result[y][x] = static_cast<T>(x == y);
+      }
+    }
+
+    return result;
+  }
+};
+
+typedef matrix<4, 4, float> mat4x4f;
+
+template <uint32 TDim, typename T>
+vec<TDim, T> operator *(const matrix<TDim, TDim, T>& mat,
+                        const vec<TDim, T>& v)
+{
+  vec<TDim, T> result;
+  for (uint32 i = 0; i < TDim; ++i)
+  {
+    result[i] = dot(mat[i], v);
+  }
+
+  return result;
+}
+
+template<uint32 THeight, uint32 TWidth, uint32 TCommonDim, typename T>
+matrix<THeight, TWidth, T> operator *(
+    const matrix<THeight, TCommonDim, T>& left,
+    const matrix<TCommonDim, TWidth, T>& right)
+{
+  matrix<THeight, TWidth, T> result;
+  for (uint32 y = 0; y < THeight; ++y)
+  {
+    for (uint32 x = 0; x < TWidth; ++x)
+    {
+      result[y][x] = dot(left[y], right.column(x));
+    }
+  }
+
+  return result;
+}
